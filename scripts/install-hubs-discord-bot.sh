67d46645a3fb8f46000314934f5483fad81564f6
Greg Fodor <gfodor@gmail.com>
2020-02-15 02:34:05 +0000

    Fix up discord bot install

--- scripts/install-hubs-discord-bot.sh
+++ scripts/install-hubs-discord-bot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+bio svc key generate hubs-discord-bot.default $(cat /var/lib/polycosm/stack_name || echo "polycosm")
+chmod 440 /hab/cache/keys/hubs-discord-bot*
+chown root:hab /hab/cache/keys/hubs-discord-bot*
 bio svc load -s at-once -u https://bldr.reticulum.io --channel polycosm-stable mozillareality/hubs-discord-bot
 bio svc stop mozillareality/ita
 sleep 3
