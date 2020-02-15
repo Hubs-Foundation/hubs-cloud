bf3b4b86c4b530a98c75707079d3c3f2e3cd4245
Greg Fodor <gfodor@gmail.com>
2020-02-15 02:22:33 +0000

    Add discord bot install script

--- scripts/install-hubs-discord-bot.sh
+++ scripts/install-hubs-discord-bot.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-echo "WIP"
+bio svc load -s at-once -u https://bldr.reticulum.io --channel polycosm-stable mozillareality/hubs-discord-bot
+bio svc stop mozillareality/ita
+sleep 3
+bio svc start mozillareality/ita
