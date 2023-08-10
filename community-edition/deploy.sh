b39be1144a6766e9ae09a4bf691f599a3b3bb379
Brandon Patterson <lilamar31@gmail.com>
2023-08-10 12:38:16 -0400

    wip

--- community-edition/deploy.sh
+++ community-edition/deploy.sh
@@ -1,53 +1,39 @@
 #!/bin/bash
 
-read -p "Enter namespace: " namespace
-read -p "Enter HubDomain: " hub_domain
-read -p "Enter DBpass: " db_pass
-read -p "Enter SmtpServer: " smtp_server
-read -p "Enter SmtpPort: " smtp_port
-read -p "Enter SmtpUser: " smtp_user
-read -p "Enter SmtpPass: " smtp_pass
-read -p "Enter UserEmail: " user_email
-read -p "Enter JWK: " jwk
-read -p "Enter GuardianKey: " guardian_key
-read -p "Enter PhxKey: " phx_key
-read -p "Enter SKETCHFAB_API_KEY: " sketchfab_api_key
-read -p "Enter TENOR_API_KEY: " tenor_api_key
-read -p "Enter PSQL: " psql
+# Read user inputs
+declare -A user_inputs
+prompts=("namespace" "HubDomain" "DBpass" "SmtpServer" "SmtpPort" "SmtpUser" "SmtpPass" "UserEmail" "GuardianKey" "PhxKey" "SKETCHFAB_API_KEY" "TENOR_API_KEY")
 
+for prompt in "${prompts[@]}"; do
+    read -p "Enter $prompt: " input
+    user_inputs["$prompt"]=$input
+done
+
+# Create private and public keys.
+openssl rsa -pubout -in private_key.pem -out public_key.pem
+
+base64_private_key=$(cat private_key.pem | base64 -w 0)
+base64_public_key=$(cat public_key.pem | base64 -w 0)
+
+perms_key="$base64_private_key"
+
+jwk="$base64_public_key" 
+
+# Update placeholders in the YAML file
 yaml_file="chart.yaml"
+temp_file="temp_chart.yaml"
 
-# Update the placeholders in the YAML file using sed
-awk -v ns="$namespace" \
-    -v hd="$hub_domain" \
-    -v dbp="$db_pass" \
-    -v pk="$perms_key" \
-    -v ss="$smtp_server" \
-    -v sp="$smtp_port" \
-    -v su="$smtp_user" \
-    -v spw="$smtp_pass" \
-    -v ue="$user_email" \
-    -v jw="$jwk" \
-    -v gk="$guardian_key" \
-    -v ph="$phx_key" \
-    -v sk="$sketchfab_api_key" \
-    -v ta="$tenor_api_key" \
-    -v psql="$psql" \
-    '{
-        gsub("{{Namespace}}", ns);
-        gsub("{{HubDomain}}", hd);
-        gsub("{{DBpass}}", dbp);
-        gsub("{{PermsKey}}", pk);
-        gsub("{{SmtpServer}}", ss);
-        gsub("{{SmtpPort}}", sp);
-        gsub("{{SmtpUser}}", su);
-        gsub("{{SmtpPass}}", spw);
-        gsub("{{UserEmail}}", ue);
-        gsub("{{JWK}}", jw);
-        gsub("{{GuardianKey}}", gk);
-        gsub("{{PhxKey}}", ph);
-        gsub("{{SKETCHFAB_API_KEY}}", sk);
-        gsub("{{TENOR_API_KEY}}", ta);
-        gsub("{{PSQL}}", psql);
-        print;
-    }' "$yaml_file" > tmpfile && mv tmpfile "$yaml_file"
+awk -v perms_key="$perms_key" '{ 
+    gsub("{{PermsKey}}", perms_key);
+    print;
+}' "$yaml_file" > "$temp_file" && mv "$temp_file" "$yaml_file"
+
+awk -v jwk="$jwk" '{ 
+    gsub("{{JWK}}", jwk);
+    print;
+}' "$yaml_file" > "$temp_file" && mv "$temp_file" "$yaml_file"
+
+for prompt in "${prompts[@]}"; do
+    value="${user_inputs[$prompt]}"
+    sed -i "s/{{$prompt}}/$value/g" "$yaml_file"
+done
