a2c86530ace54c03e53c8d6321620a82684108cf
tanfarming <tanfarming@hotmail.com>
2023-10-30 15:42:45 -0400

    word

--- community-edition/render_hcce.sh
+++ community-edition/render_hcce.sh
@@ -14,32 +14,27 @@ fi
 
 
 ### required
-export HUB_DOMAIN="example.net"
-export ADM_EMAIL="admin@example.net"
+[ -z "$HUB_DOMAIN" ] && export HUB_DOMAIN="example.net"
+[ -z "$ADM_EMAIL" ]  && export ADM_EMAIL="admin@example.net"
 
-export Namespace="hcce"
+[ -z "$Namespace" ] && export Namespace="hcce"
 
-export DB_USER="postgres"
-export DB_PASS="123456"
-export DB_NAME="retdb"
-export DB_HOST="pgbouncer"
-export DB_HOST_T="pgbouncer-t"
-export PGRST_DB_URI="postgres://$DB_USER:$DB_PASS@pgbouncer/$DB_NAME"
-export PSQL="postgres://$DB_USER:$DB_PASS@pgbouncer/$DB_NAME"
+[ -z "$DB_USER" ] && export DB_USER="postgres"
+[ -z "$DB_PASS" ] && export DB_PASS="123456"
+[ -z "$DB_NAME" ] && export DB_NAME="retdb"
+[ -z "$DB_HOST" ] && export DB_HOST="pgbouncer"
 
-export SMTP_SERVER="changeMe"
-export SMTP_PORT="changeMe"
-export SMTP_USER="changeMe"
-export SMTP_PASS="changeMe"
+[ -z "$SMTP_SERVER" ] && export SMTP_SERVER="changeMe"
+[ -z "$SMTP_PORT" ]   && export SMTP_PORT="changeMe"
+[ -z "$SMTP_USER" ]   && export SMTP_USER="changeMe"
+[ -z "$SMTP_PASS" ]   && export SMTP_PASS="changeMe"
 
-export NODE_COOKIE="changeMe"
-export GUARDIAN_KEY="changeMe"
-export PHX_KEY="changeMe"
+[ -z "$NODE_COOKIE" ]  && export NODE_COOKIE="changeMe"
+[ -z "$GUARDIAN_KEY" ] && export GUARDIAN_KEY="changeMe"
+[ -z "$PHX_KEY" ]      && export PHX_KEY="changeMe"
 
-export SKETCHFAB_API_KEY="?"
-export TENOR_API_KEY="?"
-
-
+[ -z "$SKETCHFAB_API_KEY" ] && export SKETCHFAB_API_KEY="changeMe"
+[ -z "$TENOR_API_KEY" ]     && export TENOR_API_KEY="changeMe"
 
 ### dev only keys
 # export PERMS_KEY='-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCerz7tPxgtwm2M\\nSFVrh+0bYzLiIo/s83xuCreKL5feMnJWoV3dhjNRhvZCsi5yNdDVZQzfe8t2dmse\\nujxtP1O1Pmg5alEksGzEHjLtxboVn4lRhkkQ2L1Rle2Cr0neLUjLfV0ek+agxxWR\\n62pNUjSxDua+gW/Uq8VveEMqLQGIRGFiAjI89cXdthW8imV32NyrIPce9WKyL3Us\\nLPXUmZwzOTV9/0U14SJm848W0DDckBQECpEG0MTmLn29J4Sq4EEky6QUD7GPkIGT\\nHRhFRQEc4lgMH7Rgo1zH1HbwUCgG9bH4iPteViBrPC3SnSVsXrUO3alH1Gv56Zpe\\nQs6FHm7HAgMBAAECggEASh+IIGBJlg5tB4s+Q3WB3zouKY2Fd2ShKfHdnDHxGbys\\nxiSoaPLoA9wsKil7IqRawWNraPU1rEdScE8ELO/Y1R+qpa8w6hdzJwUIOyMScklM\\nZoV4meL0RCjpghMJSTwT9eHAXqktoMp+G+RAio+sx0wzoqdpqoj2N8SQcyIExjaW\\nWK3Na0D0ARWX5e6bmgJNoOcaJjW9eKX/m/nRgnUkJG7lNic1szXhSHJY4j/mLn+E\\nH1zIaZDVpgXA6RG8YEO9QHfJTAFsEvEO2shjVlswOBD9++FNsM69fLUE6THBugJx\\n5PtM8LdjXiFHO5G8z7BmBZWeY/s+dyfKL59UB40yKQKBgQDTUQzWnLol3jEmtuRh\\nSfJersNDNtblyX2v7uXPqUkH1yY7+GQKCh9R4to8zBXPp9FMB6tiCZ2+syP9ZJK3\\nIQZiqHpdR1zZOFHDbDr63hDuNbkEkACRDA6zhssyVFWeYSadhaS5lFA1CjSrSDZ9\\np+NRSFfpLrJQQ7rdpbNNTJ0SdQKBgQDAPSCw/Uutwy3bA8EdVjdxm45a6SpSzyBe\\nxjwXAXjQjmQG0Vu+m8rgE2jUNucIGcdhh6yp46Q95QHuLhg09P+QD4vw8aeYzuK9\\nnjaqrB0ynH3C7rOgHTWRs51JaFW7IVziBygLMukkvb0Qc+5qrRlKyeD6Bz2ZrAuN\\nr+BDktwcywKBgQCcDpwrlh0GwDuOOr0YeDLM58V+Su6TTqVKm2QOYxdy+dnbqgm/\\nPFB6+cxv38wvyeCQhI113mInpkZq6K5NHc+IZqHPZ1dTd/syFijMgdkBAp27l9lD\\nRSNKQ76mUY/VYivRYxQOlVBSi1HwOGk9jRIcQg/iPkEjc5F6BNgQuwa4zQKBgAgG\\nhUbeTDqE23U5QuamosnrZJYHBe1PGFrci8WqGhEa4LIoK1xZxK52IPo7EIoWCWzY\\n7SxqhIDQ/rOi2P/c+o5ZF86HSCfo+y5PXJjjdpSWU+m/bWBR19qtVPOrl2ioi+gj\\nxFgzV+hgw3PcYyew8k1dADdv9fJkbFcN8J7hkq7hAoGAWWCIX0N8K/u6Qy7YYgTg\\nmNJqXusFDQFGizM6A3mfziIpMi9vsUDNY4D7VHwIKStbei78Jf/Em3MyxxNAb78z\\nW0hs6TeiTgbZQFBcktrDoDGQ34kZ/1jIFS1c2M/VfydvAdo36aB03SU6G0oWIlYk\\nxBclLooIqHBPs/bnYHu4xeM=\\n-----END PRIVATE KEY-----\\n'
@@ -52,6 +47,7 @@ openssl ec -pubout -in private_key.pem -out public_key.pem
 #sudo apt-get install npm && sudo npm install -g pem-jwk
 export PGRST_JWT_SECRET=$(pem-jwk public_key.pem)
 
+printenv
 ### initial cert
 openssl req -x509 -newkey rsa:2048 -sha256 -days 36500 -nodes -keyout key.pem -out cert.pem -subj '/CN='$HUB_DOMAIN
 export initCert=$(base64 -i cert.pem | tr -d '\n')
