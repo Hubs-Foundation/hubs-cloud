ed18fd9d80c65bdd901b0a6eaba05dcd06120176
Geng Tan <tanfarming@hotmail.com>
2023-12-14 13:12:46 -0500

    Ubuntu bionic (18.04) moving to apt-archive.postgresql.org
    as per 
    https://www.postgresql.org/message-id/ZN4OigxPJA236qlg%40msg.df7cb.de

--- scripts/turkey-pack-n-go.sh
+++ scripts/turkey-pack-n-go.sh
@@ -40,7 +40,7 @@ echo "native-hub-domain (ie: foobar.myhubs.net):" && read hubdomain
 echo -e "using hubdomain: $hubdomain \n"
 
 # update psql
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb http://apt-archive.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
 sudo apt update
 sudo apt install postgresql -y
