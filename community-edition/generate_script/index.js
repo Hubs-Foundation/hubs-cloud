11f31525996ddeb8fd4496939fbb5e3b923a5bd2
Ryan Inch <mythologylover75@gmail.com>
2024-09-05 03:25:21 -0400

    Add support for generating persistent volumes.
    The persistent volume settings can be configured in input-values.yaml, including whether they're generated or not.
    Persistent volumes are based off of the persistent_volumes.yam template file.
    The yaml library has been swapped out for one that will better preserve comments and formatting.
    Note: The commented out speelycaptor section in generate_script/hcce.yam is now enclosed within a document to prevent it from getting stripped out when adding persistent volumes.

--- community-edition/generate_script/index.js
+++ community-edition/generate_script/index.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 const forge = require("node-forge");
 const path = require("path");
-const yaml = require("js-yaml");
+const YAML = require("yaml");
 const pemJwk = require("pem-jwk");
 const utils = require("../utils");
 
@@ -60,12 +60,39 @@ function convertPemToJwk(publicKey) {
   return JSON.stringify(jwk);
 }
 
+function generatePersistentVolumes(processedConfig, replacedContent) {
+  const yamlDocuments = YAML.parseAllDocuments(replacedContent);
+  const persistent_volumes_template = utils.readTemplate("/generate_script", "persistent_volumes.yam");
+  const replacedPersistentVolumesContent = utils.replacePlaceholders(persistent_volumes_template, processedConfig);
+
+  // Add in the persistent volume configs to the hcce.yaml file
+  YAML.parseAllDocuments(replacedPersistentVolumesContent).forEach((doc, index) => {
+    yamlDocuments.splice(2 + index, 0, doc);
+  });
+
+  // update the volume specifications for pgsql and reticulum to point to the persistent volumes
+  yamlDocuments.forEach((doc, index) => {
+    const jsDoc = doc.toJS();
+    if (jsDoc.kind === "Deployment" && jsDoc.metadata.name === "pgsql") {
+      jsDoc.spec.template.spec.volumes[0] = {"name": "postgresql-data", "persistentVolumeClaim": {"claimName": "pgsql-pvc"}};
+      yamlDocuments[index] = new YAML.Document(jsDoc);
+    }
+    if (jsDoc.kind === "Deployment" && jsDoc.metadata.name === "reticulum") {
+      jsDoc.spec.template.spec.volumes[0] = {"name": "storage", "persistentVolumeClaim": {"claimName": "ret-pvc"}};
+      yamlDocuments[index] = new YAML.Document(jsDoc);
+    }
+  });
+
+  return `${yamlDocuments.map(doc => YAML.stringify(doc, {"lineWidth": 0, "directives": false})).join('---\n')}`;
+}
+
 // Main function to handle the script
 function main() {
   try {
     const config = utils.readConfig();
-    const processedConfig = yaml.load(
-      utils.replacePlaceholders(yaml.dump(config), config)
+    const processedConfig = YAML.parse(
+      utils.replacePlaceholders(YAML.stringify(config), config),
+      {"schema": "yaml-1.1"} // required to load yes/no as boolean values
     );
 
     // Generate keys and certificate
@@ -77,10 +104,16 @@ function main() {
     processedConfig.initCert = Buffer.from(pemCert).toString('base64').replace(/\n/g, "");
     processedConfig.initKey = Buffer.from(pemPrivateKey).toString('base64').replace(/\n/g, "");
 
+    // generate the hcce.yaml file
     const template = utils.readTemplate("/generate_script", "hcce.yam");
-    const replacedContent = utils.replacePlaceholders(template, processedConfig);
+    var replacedContent = utils.replacePlaceholders(template, processedConfig);
+
+    if (processedConfig.GENERATE_PERSISTENT_VOLUMES) {
+      replacedContent = generatePersistentVolumes(processedConfig, replacedContent);
+    }
+
     utils.writeOutputFile(replacedContent, "", "hcce.yaml");
-    console.log("Environment variables set and keys generated successfully.");
+
   } catch (error) {
     console.error("Error in main function:", error);
   }
