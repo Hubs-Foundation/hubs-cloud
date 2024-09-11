05050f5d80d30c9e8b753284ea76be1982529dde
P. Douglas Reeder <reeder.29@gmail.com>
2024-09-11 17:00:58 -0400

    Adds script to retrieve external IP address of load balancer, for DNS

---
+++ community-edition/get_ip/index.js
@@ -0,0 +1,13 @@
+const utils = require("../utils");
+const { exec } = require('node:child_process');
+
+const config = utils.readConfig();
+const cmdline = `kubectl -n ${config.Namespace} get svc lb -o json`;
+exec(cmdline, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  const output = JSON.parse(stdout);
+  console.log(output.status.loadBalancer.ingress[0].ip);
+});
