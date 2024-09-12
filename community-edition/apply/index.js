4f8e14cf4dfd6e9f40516536d659ad8a37dd7d5c
P. Douglas Reeder <reeder.29@gmail.com>
2024-09-12 09:22:57 -0400

    'apply' script also checks if replicas are updated & available

--- community-edition/apply/index.js
+++ community-edition/apply/index.js
@@ -1,10 +1,10 @@
+const { spawn } = require('node:child_process');
 const utils = require("../utils");
 const util = require('node:util');
-const exec = util.promisify(require('node:child_process').exec);
+const execFile = util.promisify(require('node:child_process').execFile);
 
 main().catch(err => {
   if (err.stderr) {
-    console.log(err.stdout)
     console.error(err.stderr);
   } else {
     console.error(err);
@@ -12,20 +12,29 @@ main().catch(err => {
 });
 
 async function main() {
-  const { stdout, stderr } = await exec("kubectl apply -f hcce.yaml");
-  console.error(stderr);
-  console.log(stdout);
+  await new Promise((resolve, reject) => {
+    const child = spawn('kubectl', ['apply', '-f', 'hcce.yaml']);
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    child.on('exit', resolve);
+    child.on('error', reject);
+  });
 
   const config = utils.readConfig();
-  const cmdline = `kubectl -n ${config.Namespace} get deployment -o json`;
+  const cmd = 'kubectl';
+  const args = ['-n', config.Namespace, 'get', 'deployment', '-o', 'json']
   const notReady = [];
   do {
-    const { stdout, stderr } = await exec(cmdline);
-      console.error(stderr);
+    const { stdout, stderr } = await execFile(cmd, args);
+      if (stderr) {
+        console.error(stderr);
+      }
       const output = JSON.parse(stdout);
       notReady.length = 0;
       for (const deployment of output.items) {
-        if (deployment.status.readyReplicas < deployment.status.replicas) {
+        if (deployment.status.readyReplicas < deployment.status.replicas ||
+          deployment.status.updatedReplicas < deployment.status.replicas ||
+          deployment.status.availableReplicas < deployment.status.replicas) {
           notReady.push(deployment.metadata.name)
         }
       }
