295a798cde94424965c18b74cf7f72ac6e622d0e
P. Douglas Reeder <reeder.29@gmail.com>
2024-09-24 15:27:22 -0400

    script 'get-ip' is now synchronous

--- community-edition/get_ip/index.js
+++ community-edition/get_ip/index.js
@@ -1,13 +1,14 @@
 const utils = require("../utils");
-const { exec } = require('node:child_process');
+const { spawnSync } = require("node:child_process");
 
 const config = utils.readConfig();
-const cmdline = `kubectl -n ${config.Namespace} get svc lb -o json`;
-exec(cmdline, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  const output = JSON.parse(stdout);
-  console.log("load balancer external IP address:", output.status.loadBalancer.ingress[0].ip);
-});
+const { stdout } = spawnSync(
+  "kubectl",
+  ["-n", config.Namespace, "get", "svc", "lb", "-o", "json"],
+  { stdio: ["pipe", "pipe", "inherit"] }
+);
+const output = JSON.parse(stdout);
+console.log(
+  "load balancer external IP address:",
+  output.status.loadBalancer.ingress[0].ip
+);
